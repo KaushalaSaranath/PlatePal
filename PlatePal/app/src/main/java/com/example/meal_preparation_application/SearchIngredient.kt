53d393459eb2888fe549831cb1af9f46a80b0400
KaushalaSaranath <kaushala.saranath7@gmail.com>
2023-04-12 15:00:04 +0530

    creating meal cards

--- PlatePal/app/src/main/java/com/example/meal_preparation_application/SearchIngredient.kt
+++ PlatePal/app/src/main/java/com/example/meal_preparation_application/SearchIngredient.kt
@@ -1,9 +1,16 @@
 package com.example.meal_preparation_application
 
-import android.content.Intent
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.util.TypedValue
+import android.view.Gravity
+import android.view.View
+import android.widget.*
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import com.example.meal_preparation_application.classes.Meals
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -18,18 +25,22 @@ import java.net.URL
 
 class SearchIngredient : AppCompatActivity() {
 
+    lateinit var scroll_linearLayout : LinearLayout
+    var allMeals: ArrayList<Meals>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_ingredient)
 
         var stb = StringBuilder()
 
-        val url_string = "https://www.themealdb.com/api/json/v1/1/search.php?s=chicken"
-        val url = URL(url_string)
-        val con: HttpURLConnection = url.openConnection() as HttpURLConnection
         val RetriveMeal = findViewById<Button>(R.id.retrieveId)
+        val search_field = findViewById<EditText>(R.id.searchId)
+        scroll_linearLayout = findViewById<LinearLayout>(R.id.scroll_layout)
 
         RetriveMeal.setOnClickListener{
+            val url_string = "https://www.themealdb.com/api/json/v1/1/search.php?s="+ search_field.text
+            val url = URL(url_string)
+            val con: HttpURLConnection = url.openConnection() as HttpURLConnection
             runBlocking {
                 launch {
                     // run the code of the coroutine in a new thread
@@ -41,17 +52,181 @@ class SearchIngredient : AppCompatActivity() {
                             line = bf.readLine()
                         }
                         parseJSON(stb)
+                        runOnUiThread{
+                            createMealCards()
+                        }
                     }
 
                 }
             }
         }
 
-    }suspend fun parseJSON(stb: java.lang.StringBuilder) {
+    }
+
+    private fun createMealCards() {
+        for (i in 1..(allMeals?.size!!)) {
+
+            val cardView = CardView(this)
+            val layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            layoutParams.setMargins(25, 15, 25, 0)
+            cardView.layoutParams = layoutParams
+            cardView.cardElevation = 30f
+            cardView.radius = 35f
+
+            // Create the child views for the CardView
+            val linearLayout = LinearLayout(this)
+            linearLayout.orientation = LinearLayout.VERTICAL
+            linearLayout.setPadding(15, 25, 15, 0)
+
+            val imageView = ImageView(this)
+            imageView.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                460
+            )
+            imageView.setPadding(5, 30, 5, 0)
+            imageView.setImageResource(R.drawable.testfood)
+            linearLayout.addView(imageView)
+
+            val dishNameTextView = TextView(this)
+            dishNameTextView.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            dishNameTextView.text = "TextView"
+            dishNameTextView.textAlignment = View.TEXT_ALIGNMENT_CENTER
+            dishNameTextView.setTextColor(ContextCompat.getColor(this, R.color.black))
+            dishNameTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20f)
+            dishNameTextView.setTypeface(Typeface.DEFAULT_BOLD)
+
+            linearLayout.addView(dishNameTextView)
+
+            val categoryTextView = TextView(this)
+            categoryTextView.text = "Category : "
+            categoryTextView.setTextColor(ContextCompat.getColor(this, R.color.black))
+            categoryTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
+            categoryTextView.setPadding(5, 10, 0, 0)
+
+            linearLayout.addView(categoryTextView)
+
+            val areaTextView = TextView(this)
+            areaTextView.text = "Area : "
+            areaTextView.setTextColor(ContextCompat.getColor(this, R.color.black))
+            areaTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
+            areaTextView.setPadding(5, 10, 0, 0)
+
+            linearLayout.addView(areaTextView)
+
+            val tagsTextView = TextView(this)
+            tagsTextView.text = "Tags : "
+            tagsTextView.setTextColor(ContextCompat.getColor(this, R.color.black))
+            tagsTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
+            tagsTextView.setPadding(5, 10, 0, 0)
+
+            linearLayout.addView(tagsTextView)
+
+            val drinkAlternateTextView = TextView(this)
+            drinkAlternateTextView.text = "Drink Alternate : "
+            drinkAlternateTextView.setTextColor(ContextCompat.getColor(this, R.color.black))
+            drinkAlternateTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
+            drinkAlternateTextView.setPadding(5, 10, 0, 0)
+
+            linearLayout.addView(drinkAlternateTextView)
+
+            val InstructionTextView = TextView(this)
+            InstructionTextView.text = "Instructions : "
+            InstructionTextView.setTextColor(ContextCompat.getColor(this, R.color.black))
+            InstructionTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
+            InstructionTextView.setPadding(5, 10, 0, 0)
+
+            linearLayout.addView(InstructionTextView)
+
+
+
+            val sourceTextView = TextView(this)
+            sourceTextView.text = "Source : "
+            sourceTextView.setTextColor(ContextCompat.getColor(this, R.color.black))
+            sourceTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
+            sourceTextView.setPadding(5, 10, 0, 0)
+
+            linearLayout.addView(sourceTextView)
+
+            val youtubeTextView = TextView(this)
+            youtubeTextView.text = "YouTube : "
+            youtubeTextView.setTextColor(ContextCompat.getColor(this, R.color.black))
+            youtubeTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
+            youtubeTextView.setPadding(5, 10, 0, 0)
+
+            linearLayout.addView(youtubeTextView)
+
+            val imageSourceTextView = TextView(this)
+            imageSourceTextView.text = "Image Source : "
+            imageSourceTextView.setTextColor(ContextCompat.getColor(this, R.color.black))
+            imageSourceTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
+            imageSourceTextView.setPadding(5, 10, 0, 0)
+
+            linearLayout.addView(imageSourceTextView)
+
+
+            val creativeCommonsConfirmedTextView = TextView(this)
+            creativeCommonsConfirmedTextView.text = "Creative CommonsConfirmed : "
+            creativeCommonsConfirmedTextView.setTextColor(ContextCompat.getColor(this, R.color.black))
+            creativeCommonsConfirmedTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
+            creativeCommonsConfirmedTextView.setPadding(5, 10, 0, 0)
+
+            linearLayout.addView(creativeCommonsConfirmedTextView)
+
+            val dateModifiedTextView = TextView(this)
+            dateModifiedTextView.text = "Date Modified : "
+            dateModifiedTextView.setTextColor(ContextCompat.getColor(this, R.color.black))
+            dateModifiedTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
+            dateModifiedTextView.setPadding(5, 10, 0, 0)
+
+            linearLayout.addView(dateModifiedTextView)
+//
+//
+//            val instructionsTextView = TextView(this)
+//            instructionsTextView.layoutParams = LinearLayout.LayoutParams(
+//                LinearLayout.LayoutParams.MATCH_PARENT,
+//                LinearLayout.LayoutParams.WRAP_CONTENT
+//            )
+//            instructionsTextView.text = "Instructions"
+//            instructionsTextView.setTextColor(Color.parseColor("#000000"))
+//            instructionsTextView.setTypeface(Typeface.DEFAULT, Typeface.ITALIC)
+//
+//            linearLayout.addView(instructionsTextView)
+//
+//            val cookingTextView = TextView(this)
+//            cookingTextView.layoutParams = LinearLayout.LayoutParams(
+//                LinearLayout.LayoutParams.MATCH_PARENT,
+//                LinearLayout.LayoutParams.WRAP_CONTENT
+//            )
+//            cookingTextView.text = "cooking meal"
+//            cookingTextView.setTextColor(Color.parseColor("#000000"))
+//            cookingTextView.setTypeface(Typeface.DEFAULT)
+//
+//            linearLayout.addView(cookingTextView)
+
+//            val measureTextView = TextView(this)
+//            measureTextView.layoutParams = LinearLayout.LayoutParams(
+//                154,
+//                LinearLayout.LayoutParams.WRAP_CONTENT
+//            )
+//            measureTextView.text = "100g"
+//            measureTextView.setTextColor(Color.parseColor("#000000"))
+//            measureTextView.setTypeface(Typeface.DEFAULT, Typeface.ITALIC)
+            cardView.addView(linearLayout)
+            scroll_linearLayout.addView(cardView)
+        }
+    }
+
+    suspend fun parseJSON(stb: java.lang.StringBuilder) {
 // this contains the full JSON returned by the Web Service
         val json = JSONObject(stb.toString())
 // Information about all the books extracted by this function
-        var allMeals = ArrayList<Meals>()
+        allMeals = ArrayList<Meals>()
         var jsonArray: JSONArray = json.getJSONArray("meals")
 // extract all the books from the JSON array
         for (i in 0..jsonArray.length()-1) {
@@ -74,7 +249,7 @@ class SearchIngredient : AppCompatActivity() {
                 creativeCommonsConfirmed = food["strCreativeCommonsConfirmed"] as? String ?: null,
                 dateModified = food["dateModified"] as? String ?: null,
             )
-            allMeals.add(tempMeal)
+            allMeals!!.add(tempMeal)
 // extract all the authors
         }
         println("data stored")
